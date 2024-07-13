82a6ff1a2176f381c1d481635a7c89ecb08457f7
mBentaifour <bentaifourmoh@gmail.com>
2024-07-13 09:28:09 -0700

    Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 			{
 				temp = array[k];
 				array[k] = array[k + 1];
-				array[k + 1] - temp;
+				array[k + 1] = temp;
 				print_array(array, size);
 			}
 		}
